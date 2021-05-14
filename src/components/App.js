edfb23f1e57ac16c31420f2edcf3a7cf81545052
CShah44 <chaityashah36@gmail.com>
2021-05-14 20:53:44 +0530

    Worked on context, signup functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import SignUp from "./SignUp";
+
 function App() {
-  return "Hello World";
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignUp />
+        </div>
+      </Container>
+    </AuthProvider>
+  );
 }
 
 export default App;
